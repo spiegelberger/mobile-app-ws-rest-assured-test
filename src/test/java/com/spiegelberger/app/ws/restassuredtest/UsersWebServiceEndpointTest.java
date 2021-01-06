815d7cd2bdd8beec1142a31b6f2768f1399cf49a
Zsolt Spiegelberger <zsoltspiegelberger@gmail.com>
2021-01-06 07:28:06 +0100

    testGetUserDetails test first version which results in StackOverflowError

--- src/test/java/com/spiegelberger/app/ws/restassuredtest/UsersWebServiceEndpointTest.java
+++ src/test/java/com/spiegelberger/app/ws/restassuredtest/UsersWebServiceEndpointTest.java
@@ -1,21 +1,30 @@
 package com.spiegelberger.app.ws.restassuredtest;
 
 import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import org.junit.FixMethodOrder;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.runners.MethodSorters;
 
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 class UsersWebServiceEndpointTest {
 	
 	private final String CONTEXT_PATH = "/mobile-app-ws";
 	private final String EMAIL="zsoltspiegelberger@gmail.com";
+	private final String JSON = "application/json";
+	private static String authorizationHeader;
+	private static String userId;
 
 	@BeforeEach
 	void setUp() throws Exception {
@@ -23,27 +32,68 @@ class UsersWebServiceEndpointTest {
 		RestAssured.port=8080;
 	}
 
+	
+	/*
+	 * testUserLogin()
+	 */
 	@Test
-	void testUserLogin() {
+	void a() {
 				
 		Map<String, String>loginDetails= new HashMap<>();
 		loginDetails.put("email", EMAIL);
 		loginDetails.put("password", "123");
 		
 		Response response = given()
-		.contentType("application/json")
-		.accept("application/json")
+		.contentType(JSON)
+		.accept(JSON)
 		.body(loginDetails)
 		.when().post(CONTEXT_PATH + "/users/login")
 		.then()
 		.statusCode(200).extract().response();
 		
-		String authorizationHeader = response.header("Authorization");
-		String userId = response.header("UserID");
+		authorizationHeader = response.header("Authorization");
+		userId = response.header("UserID");
 		
 		assertNotNull(authorizationHeader);
 		assertNotNull(userId);
 				
 	}
-
+	
+	
+	
+	/*
+	 * testGetUserDetails()
+	 */
+/*	@Test
+	final void b() {
+		
+		Response response = given()
+				 .pathParam("id", userId)
+				 .header("Authorization",authorizationHeader)
+				 .accept(JSON)
+				 .when()
+				 .get(CONTEXT_PATH + "/users/{id}")
+				 .then()
+				 .statusCode(200)
+				 .contentType(JSON)
+				 .extract()
+				 .response();
+				
+				String userPublicId = response.jsonPath().getString("userId");
+				String userEmail = response.jsonPath().getString("email");
+		        String firstName = response.jsonPath().getString("firstName");
+		        String lastName = response.jsonPath().getString("lastName");
+		        List<Map<String, String>> addresses = response.jsonPath().getList("addresses");
+		        String addressId = addresses.get(0).get("addressId");
+				
+				assertNotNull(userPublicId);
+				assertNotNull(userEmail);
+				assertNotNull(firstName);
+				assertNotNull(lastName);
+				assertEquals(EMAIL, userEmail);
+				
+				assertTrue(addresses.size() == 2);
+				assertTrue(addressId.length() == 30);
+	}
+*/
 }
